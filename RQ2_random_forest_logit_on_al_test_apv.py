7b66850dabefc2e9560962ae5fa55e0d82f60555
MetricThresholds <125939065+MetricThresholds@users.noreply.github.com>
2023-03-12 10:07:48 +0800

    应用随机森林的方法用前一版本的真实标签数据训练模型，然后在当前版本与TAL相同测试集的数据上预测性能，以方便比较
    应用随机森林的方法用前一版本的真实标签数据训练模型，然后在当前版本与TAL相同测试集的数据上预测性能，以方便比较

--- RQ2_random_forest_logit_on_al_test_apv.py
+++ RQ2_random_forest_logit_on_al_test_apv.py
@@ -4,9 +4,7 @@
 Project: Object-oriented-Metric-Thresholds
 File: RQ2_random_forest_logit_on_al_test_apv.py
 Date : 2022/11/25 20:48
-Author : njumy
-HomePage : https://github.com/meiyuanqing
-Email : dg1533019@smail.nju.edu.cn
+
 应用随机森林的方法用前一版本的真实标签数据训练模型，然后在当前版本与TAL相同测试集的数据上预测性能，以方便比较
 """
 
